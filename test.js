e5c4fc4a5c9a93144362a2269cfeb15656e89536
Mothukuri Mani Deep <110282076+2100030898-manideep@users.noreply.github.com>
2024-04-05 11:28:19 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+Checking Output Using Remix IDE and MetaMask:
+1.Connect to Remix IDE: Open Remix IDE (https://remix.ethereum.org/) 
+2.Import Contracts: Import the Solidity contract file (MyContract.sol) into Remix.
+3.Compile Contracts: Compile the imported contract in Remix.
+4.Deploy Contracts: Deploy the compiled contract using MetaMask as the wallet provider. Ensure that MetaMask is connected to the same network where the contracts were deployed.
+5.Interact with Contracts: After deployment, interact with the deployed contract using Remix IDE and MetaMask to test its functionality.
+6.Check Output: Use Remix IDE's debugging and testing features to verify the output and behavior of the deployed contract.
