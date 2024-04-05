341ee4615dceccd3373cb933b32c02cefec39cfb
Mothukuri Mani Deep <110282076+2100030898-manideep@users.noreply.github.com>
2024-04-05 11:25:59 +0530

    Update and rename Scripts to compile.js

---
+++ compile.js
@@ -0,0 +1,4 @@
+Compilation Steps:
+---->Install Dependencies: Ensure that Node.js and npm are installed on your system.
+----?Install Ethereum Development Tools: Install tools like Truffle or Hardhat globally.
+---->Compile Contracts: Use a script (compile.js) to compile Solidity contracts. This script should use the appropriate compiler version specified in truffle-config.js or hardhat.config.js.
